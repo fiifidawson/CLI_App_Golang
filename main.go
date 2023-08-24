cb4644beb64430eda5d669b49e6f94939047c4e9
Fiifi Dawson <edem.fiifi.dawson@gmail.com>
2023-08-24 13:11:47 +0000

    Update main.go

--- main.go
+++ main.go
@@ -5,17 +5,20 @@ import "fmt"
 func main () {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
-	var remainingTickets int = 50
+	var remainingTickets uint = 50
+	//Array
+	bookings := []string{}
 
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+
 	// Defining the type
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Printf("Enter your first name:")
 	// User input
@@ -30,8 +33,12 @@ func main () {
 	fmt.Printf("Enter number of tickets:")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+	
+	// Slice
+	bookings = append(bookings, firstName + "" + lastName)
 
 	fmt.Printf("Thank you %v %v  for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-
+	fmt.Printf("%v tickets are remaining for %v!", remainingTickets, conferenceName)
 		
 }
